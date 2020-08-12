cd8a83282e83fc28b42317d9c2355a920711a21e
TienTho98 <50016575+HoozukinoReitetsu@users.noreply.github.com>
2020-08-12 11:06:58 +0700

    Update regex.java

--- regex.java
+++ regex.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class regex{
      public static void main(String[] args) {
-        String pattern= "^((https|http)?:\\/\\/|www\\.)[a-z0-9A-Z]+\\.+[a-z]{2,6}+([0-9]|\\/|\\?|[a-z]|\\=|\\-|\\.|\\@|\\!|\\$|\\#|\\^|\\&|\\*|\\+|\\_)*$";
+        String pattern= "^((https|http)?:\\/\\/|www\\.)[a-z0-9A-Z]+\\.[a-z]{2,6}([0-9a-zA-Z\\/\\?\\=\\-\\.\\@\\!\\$\\#\\^\\&\\*\\+\\_]*)$";
         String line = "https://tiki.vn/dien-thoai-may-tinh-bang/c1789?src=mega-menu";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(line);
